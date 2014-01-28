25bbb4dd72ec7b8848cdc531622fc70eb5ebeabc
Marcos Vinicius Kapp <marcos.kapp@canalvpsa.com.br>
2014-01-28 13:17:09 -0200

    CLOSED - task 26: 'undefined' quando produto base https://github.com/canalvpsa/contagem_cns/issues/issue/26

--- resources/js/kapp.dataTables.js
+++ resources/js/kapp.dataTables.js
@@ -15,11 +15,13 @@
         "bProcessing": false,
         "bServerSide": false,
         "bFilter": true,
-        "iDisplayLength": 10,
-		"bPaginate": true
+        "bPaginate": true,
+        "bInfo": true,
+        "iDisplayLength": 10
     };
 
-    var settings, selector, oTable;
+    var settings, selector, oTable, shiftHeld = false;
+    var lastIndexSelected;
 
     $.fn.kappDataTable = function (options) {
 
@@ -30,19 +32,50 @@
         selector = this.selector;
 
         if (settings.MultiRowSelection) {
-            $(selector).find('tbody tr').click(function () {
-                if ($(this).hasClass('row_selected')){               
-                    $(this).removeClass('row_selected');
+            $(selector).find('tbody tr').click(function (event) {
+                if ($(this).hasClass('row_selected')){
+                	if (event.ctrlKey == 1) {
+                		$(this).removeClass('row_selected');   
+                        lastIndexSelected = null;                 
+                	}
+                	else {
+                		$(oTable.fnSettings().aoData).each(function () {
+                            $(this.nTr).removeClass('row_selected');
+                        });
+                		$(this).addClass('row_selected');                        
+                        lastIndexSelected = $(this).index();                		
+                	}
                 }
                 else {
-                    $(this).addClass('row_selected');
+                	if(!event.ctrlKey == 1 && !event.shiftKey == 1) {
+                		$(oTable.fnSettings().aoData).each(function () {
+                            $(this.nTr).removeClass('row_selected');
+                        });         
+                        lastIndexSelected = null;
+                	}
+                	var done = false;
+                	if (event.shiftKey == 1) {
+                		event.preventDefault();
+                		if(lastIndexSelected != null) {
+                			if(lastIndexSelected < $(this).index())
+                				$(this).parent().children().slice(lastIndexSelected, $(this).index() + 1).addClass('row_selected');
+                			else
+                				$(this).parent().children().slice($(this).index(), lastIndexSelected).addClass('row_selected');   
+                			done = true;
+                			window.getSelection().removeAllRanges();
+                		}
+                	}
+                	if(!done)
+                		$(this).addClass('row_selected');
+                    lastIndexSelected = $(this).index();
                 }                
             });
         }
         else {
             $(selector).find('tbody tr').click(function () {
-                if ($(this).hasClass('row_selected'))
+                if ($(this).hasClass('row_selected')){
                     $(this).removeClass('row_selected');
+                }
                 else {
                     $(oTable.fnSettings().aoData).each(function () {
                         $(this.nTr).removeClass('row_selected');
@@ -65,12 +98,13 @@
             "bProcessing": settings.bProcessing,
             "bServerSide": settings.bServerSide,
             "bFilter": settings.bFilter,
-            "iDisplayLength" : settings.iDisplayLength,
-            "bPaginate" : settings.bPaginate
+            "bPaginate": settings.bPaginate,
+            "bInfo": settings.bInfo,
+            "iDisplayLength" : settings.iDisplayLength
         });
         
         oTable.fnSort( [ [settings.sortColumn, settings.sortOrder] ] );
-        
+
         return $.fn.kappDataTable;
     };
     
@@ -82,6 +116,10 @@
         $(oTable.fnSettings().aoData).each(function () {
             $(this.nTr).removeClass('row_selected');
         });
+    };
+    
+    $.fn.kappDataTable.destroy = function (value, index) {
+        oTable.fnDestroy();
     };
 
     $.fn.kappDataTable.ClearTable = function () {
@@ -117,18 +155,6 @@
                     objects.push(obj);
             	
             }
-        });
-        
-        return objects;
-    };
-    
-    $.fn.kappDataTable.SelectedRows = function () {
-
-        objects = new Array();
-        
-        $(oTable.fnSettings().aoData).each(function () {        	
-            if($(this.nTr).hasClass('row_selected'))            	
-            	objects.push($(this.nTr));
         });
         
         return objects;
